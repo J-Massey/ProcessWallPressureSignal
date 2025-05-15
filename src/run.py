4ee5c49b9b5b571b38221530b7be29dd2586e05d
Jonah M <masseyjmo@gmail.com>
2025-05-14 17:54:02 -0700

    Need to work on the noise rejection, and structure of the package

--- src/run.py
+++ src/run.py
@@ -6,12 +6,13 @@ Hard-coded inputs and parameters for transparency.
 import os
 import numpy as np
 
-from io import load_wallpressure
+from i_o import load_wallpressure
 from plotting import (plot_spectrum_and_modes, plot_pw_p_fs,
                        plot_filtered_spectrum, plot_filtered_diff)
 from processing import (compute_psd, propagate_error,
                         compute_duct_modes, duct_mode_freq,
                         notch_filter)
+from noise_rejection import wiener_filter_timeseries, compute_coherence_psd
 
 # === Hard-coded file paths ===
 WALL_PRESSURE_MAT      = "data/wallpressure_booman_Pa.mat"
@@ -75,6 +76,30 @@ def main():
                             os.path.join(OUTPUT_DIR, "fs_notched.png"),  'g-')
     plot_filtered_diff(spec_w, phi_filt_w, phi_filt_fs,
                        os.path.join(OUTPUT_DIR, "difference.png"))
+    
+def noise_rejection():
+    L = L0 + DELTA_L0
+    fs_w, p_w = load_wallpressure(WALL_PRESSURE_MAT)
+    f_raw_w, psd_w = compute_psd(p_w, SAMPLE_RATE)
+    spec_w = propagate_error(f_raw_w, psd_w, NU0, RHO0, U_TAU0, ERR_FRAC)
+    all_modes = compute_duct_modes(U, C, range(5), range(5), range(16), W, H, L, NU0, U_TAU0, ERR_FRAC)
+    phi_filt_w, info_w   = notch_filter(spec_w["f_nom"], spec_w["phi_nom"],
+                                        spec_w["f_min"], spec_w["f_max"], all_modes["nom"])
+
+    fs_fs, p_fs = load_wallpressure(FREESTREAM_PRESSURE_MAT, var_name="wall_pressure_fluc_Pa")
+    f_raw_fs, psd_fs = compute_psd(p_fs, SAMPLE_RATE)
+    spec_fs = propagate_error(f_raw_fs, psd_fs, NU0, RHO0, U_TAU0, ERR_FRAC)
+    modes_fs = compute_duct_modes(U, C, MODE_M, MODE_N, MODE_L, W, H, L, NU0, U_TAU0, ERR_FRAC)
+    phi_filt_fs, info_fs = notch_filter(spec_fs["f_nom"], spec_fs["phi_nom"],
+                                        spec_fs["f_min"], spec_fs["f_max"], all_modes["nom"])
+
+    plot_filtered_diff(spec_w, phi_filt_w, phi_filt_fs,
+                       os.path.join(OUTPUT_DIR, "difference.png"))
+    # analyze coherence
+    compute_coherence_psd(p_fs, p_w, SAMPLE_RATE)
+    wiener_filter_timeseries(p_fs, p_w, SAMPLE_RATE)
+
 
 if __name__ == "__main__":
-    main()
+    # main()
+    noise_rejection()
