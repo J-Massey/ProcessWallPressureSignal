7d4162cdd9f41c1e7bc196d03337066a528104a5
Jonah M <masseyjmo@gmail.com>
2025-09-15 15:47:12 -0700

    hmmm, raw not plotting...

--- src/atm_callibration.py
+++ src/atm_callibration.py
@@ -9,6 +9,7 @@ import os
 
 from plotting import (
     plot_spectrum,
+    plot_raw_spectrum,
     plot_transfer_NKD,
     plot_transfer_PH,
     plot_transfer_NC,
@@ -245,14 +246,12 @@ def main_NC():
 
 
 def real_data():
-    psi = ['atm', '10psi', '30psi', '50psi', '70psi', '100psi']
-    root = 'data/14082025/flow/maxspeed'
-    fn_sweep = [f'{root}/data_{p}.mat' for p in psi]
-    OUTPUT_DIR = "figures/real"
+    root = 'data/11092025'
+    fn_sweep = [f'{root}/data.mat' for p in psi_labels]
+    OUTPUT_DIR = "figures/cali_09/flow"
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     PH_path = _resolve_cal_dir("PH-NKD")
     NC_path = _resolve_cal_dir("NC-NKD")
-    NKD_path = _resolve_cal_dir("S1-S2")
 
     RAW_DIR = f"{OUTPUT_DIR}/raw"
     os.makedirs(RAW_DIR, exist_ok=True)
@@ -265,75 +264,69 @@ def real_data():
     Times = []
     Pyys = []
     Pyryrs = []
-    for idx in range(len(psi)):
-        ic(f"Processing {psi[idx]}...")
+    for idx in range(len(psi_labels)):
+        ic(f"Processing {psi_labels[idx]}...")
 
         # Load data
-        x_r, y_r = load_mat(fn_sweep[idx]) # s1 mic is naked, so no need to correct NKD1 to NKD2
+        x_r, y_r = load_mat(fn_sweep[idx], key='channelData_300') # NC (x_r), PH (y_r)
+        f, Pyryr = compute_spec(FS, y_r)
+        Pyryr_plus = Pyryr / ((u_taus[idx] ** 2 * rhos[idx]) ** 2)
         t = np.arange(len(y_r)) / FS
-        plot_time_series(t, y_r, f"{RAW_DIR}/y_r_{psi[idx]}")
-        plot_time_series(t, x_r, f"{RAW_DIR}/x_r_{psi[idx]}")
+        # plot_time_series(t, x_r, f"{RAW_DIR}/x_r_{psi_labels[idx]}")
+        # plot_time_series(t, y_r, f"{RAW_DIR}/y_r_{psi_labels[idx]}")
         ###
         # Correct NC (invert NC→NKD mapping to bring NC into NKD domain)
-        H_NC = np.load(f"{NC_path}/H_{psi[idx]}.npy")
-        gamma2_NC = np.load(f"{NC_path}/gamma2_{psi[idx]}.npy")
-        f_NC = np.load(f"{NC_path}/f_{psi[idx]}.npy")
-        fs = 25000.0
-        y = wiener_inverse(y_r, fs, f_NC, H_NC, gamma2_NC)
+        H_NC = np.load(f"{NC_path}/H_{psi_labels[idx]}.npy")
+        gamma2_NC = np.load(f"{NC_path}/gamma2_{psi_labels[idx]}.npy")
+        f_NC = np.load(f"{NC_path}/f_{psi_labels[idx]}.npy")
+
+        x = wiener_inverse(x_r, FS, f_NC, H_NC, gamma2_NC)
         # plot whole trace
-        plot_time_series(t, y, f"{OUTPUT_DIR}/y_{psi[idx]}")
+        # plot_time_series(t, x, f"{OUTPUT_DIR}/x_{psi_labels[idx]}")
         ###
         # Correct PH (invert PH→NKD mapping to bring PH into NKD domain) # NKD mic is S2 mic, so we need to correct NKD2 to NKD1 first
-        H_PH = np.load(f"{PH_path}/H_{psi[idx]}.npy")
-        gamma2_PH = np.load(f"{PH_path}/gamma2_{psi[idx]}.npy")
-        f_PH = np.load(f"{PH_path}/f_{psi[idx]}.npy")
-        x_s2 = wiener_inverse(x_r, fs, f_PH, H_PH, gamma2_PH)
+        H_PH = np.load(f"{PH_path}/H_{psi_labels[idx]}.npy")
+        gamma2_PH = np.load(f"{PH_path}/gamma2_{psi_labels[idx]}.npy")
+        f_PH = np.load(f"{PH_path}/f_{psi_labels[idx]}.npy")
+        y = wiener_inverse(y_r, FS, f_PH, H_PH, gamma2_PH)
         # plot whole trace
-        plot_time_series(t, x_s2, f"{OUTPUT_DIR}/x_s2_{psi[idx]}")
-        ###
-        # This has now been inverted to the equivalend of the NKD-S2 mic, we need to map it to the response of the pristine S1 mic
-        H_NKD = np.load(f"{NKD_path}/H_{psi[idx]}.npy")
-        gamma2_NKD = np.load(f"{NKD_path}/gamma2_{psi[idx]}.npy")
-        f_NKD = np.load(f"{NKD_path}/f_{psi[idx]}.npy")
-        x = wiener_inverse(x_s2, fs, f_NKD, H_NKD, gamma2_NKD)
-        # plot whole trace
-        plot_time_series(t, x, f"{OUTPUT_DIR}/x_{psi[idx]}")
-        ###
+        # plot_time_series(t, y, f"{OUTPUT_DIR}/y_{psi_labels[idx]}")
         
         # Plot corrected PH
-        f, Pxx = compute_spec(fs, x_r)
+        f, Pyy = compute_spec(FS, y)
         f_plus = f * nu_s[idx] / (u_taus[idx] ** 2)
-        Pxx_plus = Pxx / ((u_taus[idx] ** 2 * rhos[idx]) ** 2) 
+        Pyy_plus = Pyy / ((u_taus[idx] ** 2 * rhos[idx]) ** 2) 
+        ic(Pyy_plus.max())
         Times.append(1/f_plus)
-        Pyys.append(f*Pxx_plus)
-        # plot_spectrum(f, f*Pyy, f"{OUTPUT_DIR}/Pyryr_{psi[idx]}_corr")
+        Pyys.append(f*Pyy_plus)
+        plot_raw_spectrum(1/f_plus, Pyy_plus, f"{OUTPUT_DIR}/Pyy_{psi_labels[idx]}_corr")
         ###
         # TF between NC and PH
-        f, H, gamma2 = estimate_frf(x, y, fs)
+        # f, H, gamma2 = estimate_frf(x, y, FS)
         # plot_transfer_PH(f, H, f"{OUTPUT_DIR}/H_{psi[idx]}", psi[idx])
 
-        y_rej = wiener_inverse(y, fs, f, H, gamma2)
-        t = np.arange(len(y)) / fs
-        # plot_corrected_trace_PH(t, x, y, y_rej, f"{OUTPUT_DIR}/y_{psi[idx]}", psi[idx])
-        f, Pyy_rej = compute_spec(fs, y_rej)
-        f_plus = f * nu_s[idx] / (u_taus[idx] ** 2)
-        Pyy_rej_plus = Pyy_rej / ((u_taus[idx] ** 2 * rhos[idx]) ** 2)
-        Pyryrs.append(f*Pyy_rej_plus)
+    #     y_rej = wiener_inverse(y, fs, f, H, gamma2)
+    #     t = np.arange(len(y)) / fs
+    #     # plot_corrected_trace_PH(t, x, y, y_rej, f"{OUTPUT_DIR}/y_{psi[idx]}", psi[idx])
+    #     f, Pyy_rej = compute_spec(fs, y_rej)
+    #     f_plus = f * nu_s[idx] / (u_taus[idx] ** 2)
+    #     Pyy_rej_plus = Pyy_rej / ((u_taus[idx] ** 2 * rhos[idx]) ** 2)
+    #     Pyryrs.append(f*Pyy_rej_plus)
 
-    spectra_dir = os.path.join(OUTPUT_DIR, "spectra")
-    os.makedirs(spectra_dir, exist_ok=True)
-    ax1, ax2 = plot_spectrum(Times, Pyys, Pyryrs, f"{spectra_dir}/Pyy_log.png")
-    # Plot vertical lines at f = 1Hz, 25000Hz
-    for ax in [ax1, ax2]:
-        for idx in range(len(psi)):
-            ax.axvline(1 * u_taus[idx]**2 / nu_s[idx], color='k', ls='--', lw=0.5)
-            ax.axvline(1/25000.0 * u_taus[idx]**2 / nu_s[idx], color='k', ls='--', lw=0.5) 
-    plt.savefig(f"{spectra_dir}/Pyy_log.png")
-    plt.close()
+    # spectra_dir = os.path.join(OUTPUT_DIR, "spectra")
+    # os.makedirs(spectra_dir, exist_ok=True)
+    # ax1, ax2 = plot_spectrum(Times, Pyys, Pyryrs, f"{spectra_dir}/Pyy_log.png")
+    # # Plot vertical lines at f = 1Hz, 25000Hz
+    # for ax in [ax1, ax2]:
+    #     for idx in range(len(psi)):
+    #         ax.axvline(1 * u_taus[idx]**2 / nu_s[idx], color='k', ls='--', lw=0.5)
+    #         ax.axvline(1/25000.0 * u_taus[idx]**2 / nu_s[idx], color='k', ls='--', lw=0.5) 
+    # plt.savefig(f"{spectra_dir}/Pyy_log.png")
+    # plt.close()
 
 
 if __name__ == "__main__":
-    main_PH()
-    main_NC()
+    # main_PH()
+    # main_NC()
     # main_NKD()
-    # real_data()
+    real_data()
