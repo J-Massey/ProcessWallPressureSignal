ecfe5d3592a03634a81cb37db60af623f8614293
Jonah M <masseyjmo@gmail.com>
2025-11-20 16:39:09 -0800

    Plotting from data

---
+++ src/plot/from_data/F_freestreamp_SU_raw_production.py
@@ -0,0 +1,81 @@
+fps = [
+        'data/final_pressure/F_freestreamp_SU_raw.hdf5',
+        'data/final_pressure/F_freestreamp_SU_production.hdf5',
+        'data/final_pressure/G_wallp_SU_raw.hdf5',
+        'data/final_pressure/G_wallp_SU_production.hdf5',
+        
+    ]
+
+
+import os
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import welch, get_window
+import scienceplots
+from icecream import ic
+
+plt.style.use(["science", "grid"])
+plt.rcParams["font.size"] = 10.5
+plt.rc("text", usetex=True)
+plt.rc("text.latex", preamble=r"\usepackage{mathpazo}")
+
+# -------------------- constants --------------------
+FS = 50_000.0
+NPERSEG = 2**14          # keep one value for all runs
+WINDOW  = "hann"
+PSI_TO_PA = 6_894.76
+
+LABELS = ("0psig", "50psig", "100psig")
+PSIGS  = (0.0, 50.0, 100.0)
+COLOURS = ("#1e8ad8", "#ff7f0e", "#26bd26")  # hex equivalents of C0, C1, C2
+
+
+def compute_spec(x: np.ndarray, fs: float = FS, nperseg: int = NPERSEG):
+    """Welch PSD with consistent settings. Returns f [Hz], Pxx [Pa^2/Hz]."""
+    x = np.asarray(x, float)
+    nseg = min(nperseg, x.size)
+    if nseg < 16:
+        raise ValueError(f"Signal too short for Welch: n={x.size}, nperseg={nperseg}")
+    w = get_window(WINDOW, nseg, fftbins=True)
+    f, Pxx = welch(
+        x, fs=fs, window=w, nperseg=nseg, noverlap=nseg//2,
+        detrend="constant", scaling="density", return_onesided=True,
+    )
+    return f, Pxx
+
+def plot_fs_raw():
+    psigs = ['0psig', '50psig', '100psig']
+    Re_noms = [1_500, 4_500, 9_000]
+    fig, ax = plt.subplots(1, 2, figsize=(6, 3), sharey=True)
+    ax[0].set_title("NC--close run")
+    ax[1].set_title("NC--far run")
+    ax[0].set_xlabel(r"$T^+$")
+    ax[1].set_xlabel(r"$T^+$")
+    ax[0].set_ylabel(r"${f \phi_{pp}}_{\mathrm{prod.}}^+$")
+
+    with h5py.File(fps[1], 'r') as f_raw:
+        for psig in psigs:
+            nc_raw_close = f_raw[f'freestream_production/{psig}/close/NC_Pa'][:]
+            nc_raw_far = f_raw[f'freestream_production/{psig}/far/NC_Pa'][:]
+            ic(f_raw[f'freestream_production/{psig}'].attrs.keys())
+            rho = f_raw[f'freestream_production/{psig}'].attrs['rho'][()]
+            u_tau = f_raw[f'freestream_production/{psig}'].attrs['u_tau'][()]
+            nu = f_raw[f'freestream_production/{psig}'].attrs['nu'][()]
+    
+            f, Pxx_close = compute_spec(nc_raw_close, fs=FS, nperseg=NPERSEG)
+            f, Pxx_far = compute_spec(nc_raw_far, fs=FS, nperseg=NPERSEG)
+
+            T_plus = (u_tau**2) / (nu * f)
+
+            norm_factor = (rho**2) * (u_tau**4)
+            ax[0].loglog(T_plus, f * Pxx_close / norm_factor, label=fr'$Re_\tau^{{\mathrm{{nom}}}}$={Re_noms[psigs.index(psig)]}')
+            ax[1].loglog(T_plus, f * Pxx_far / norm_factor, label=psig)
+
+    
+    ax[0].legend()
+    ax[1].legend()
+    plt.savefig("figures/from_data/F_freestreamp_SU_production.png", dpi=600)
+
+if __name__ == "__main__":
+    plot_fs_raw()
